c112214473dce47627d684ae58010dd0d978fd62
Scarlettidiot <99799956+Scarlettidiot@users.noreply.github.com>
2022-05-01 13:10:18 +0800

    Update assign.cpp

--- assign.cpp
+++ assign.cpp
@@ -2,8 +2,7 @@
 #include <time.h>
 using namespace std;
 
-void select(int &arr[]){
-  int m=0,arr[4];
+void select_bad(int &arr[4]){
   srand((unsigned)time(NULL));
   for (int i=0;i<4;i++){
     arr[i]=rand()%10;//randomly select one number
@@ -13,4 +12,25 @@ void select(int &arr[]){
       }
     }
   }
+}
+
+int player_num(int &arr[4]){
+  srand((unsigned)time(NULL));
+  bool flag=1;
+  int playernum;
+  while (flag){
+    playernum=rand()%10;
+    bool flag2=0;
+    for (int i=0;i<4;i++){
+      if (arr[i]==playernum){
+        flag2=1;
+      }
+    }
+    if (flag2=0){
+      flag=0;
+    }
+  }
+}
   
+  
+//arr[4]contains 4 integer from 0 to 9 represents for bad ministers.
