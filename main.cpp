050a271bf3df283653c3a1854bdd6cbba2db0e5c
Scarlettidiot <99799956+Scarlettidiot@users.noreply.github.com>
2022-05-04 19:19:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,121 +1,17 @@
 #include <iostream>
 #include <time.h>
 #include <string>
+#include "assign.h"
 using namespace std;
 
-assign(int arr[]);//assign numbers to bad, good, player
-select()//as choose nuk the player nummbers (a parameter to determine how many)
-round_result()//determine the e whether one round is success
-game_continue()//determine whether to continue a game
-final_result()//determinfinal result
-  
-#include <iostream>
-#include <time.h>
-#include <string>
-using namespace std;
+const int badcount = 4;
+const int round = 5;
+const int rcarsize[5] = {3, 4, 4, 5, 5};
 
-int main(){
-  cout<<"Welcome to Avalon!"<<endl;
-  int arr[4];
-  assign(arr[4]);
-  cout<<"Loading successful. Game starts."<<endl;
-  cout<<"ROUND I"<<endl;
-  cout<<"please select 3 minisers to join the task"<<endl;
-  int *a=new int[3];
-  for (int i=0;i<3;i++){
-    cin>>a[i];
-  }
-  if (round_result(a)==0){
-    cout<<"FAIL"<<endl;
-  }
-  else if (round_result(a)==1){
-    cout<<"SUCCESS!"<<endl;
-  }
-  delete [] a;
-  cout<<"ROUND II"<<endl;
-  cout<<"please select 4 minisers to join the task"<<endl;
-  int *a=new int[4];
-  for (int i=0;i<4;i++){
-    cin>>a[i];
-  }
-  if (round_result(a)==0){
-    cout<<"FAIL"<<endl;
-  }
-  else if (round_result(a)==1){
-    cout<<"SUCCESS!"<<endl;
-  }
-  delete [] a;
-  if (consecutive()=1){//means that two consecutive round occurs;
-    hints();
-  }
-  cout<<"ROUND III"<<endl;
-  cout<<"please select 4 minisers to join the task"<<endl;
-  int *a=new int[4];
-  for (int i=0;i<4;i++){
-    cin>>a[i];
-  }
-  if (round_result(a)==0){
-    cout<<"FAIL"<<endl;
-  }
-  else if (round_result(a)==1){
-    cout<<"SUCCESS!"<<endl;
-  }
-  delete [] a;
-  if (consecutive()=1){//means that two consecutive round occurs;
-    hints();
-  }
-  game_continue();
-  cout<<"ROUND IV"<<endl;
-  cout<<"please select 5 minisers to join the task"<<endl;
-  int *a=new int[5];
-  for (int i=0;i<5;i++){
-    cin>>a[i];
-  }
-  if (round_result(a)==0){
-    cout<<"FAIL"<<endl;
-  }
-  else if (round_result(a)==1){
-    cout<<"SUCCESS!"<<endl;
-  }
-  delete [] a;
-  if (consecutive()=1){//means that two consecutive round occurs;
-    hints();
-  }
-  game_continue();
-  cout<<"ROUND V"<<endl;
-  cout<<"please select 5 minisers to join the task"<<endl;
-  int *a=new int[5];
-  for (int i=0;i<5;i++){
-    cin>>a[i];
-  }
-  if (round_result(a)==0){
-    cout<<"FAIL"<<endl;
-  }
-  else if (round_result(a)==1){
-    cout<<"SUCCESS!"<<endl;
-  }
-  delete [] a;
-  final_result();
-  return 0;
-}
-
-
-
-assign(int arr[]){
-  srand((unsigned)time(NULL));
-  for (int i=0;i<4;i++){
-    arr[i]=rand()%10;//randomly select one number
-    for (int k=0;k<i;k++){
-      if (arr[k]==arr[i]){
-        i--;
-      }
-    }
-  }
-}//arr[4]contains 4 integer from 0 to 9 represents for bad ministers.
 
 bool round_result(int csize, int car[], int bad[])
 {
-  for (int i = 0; i < csize, i++)
+  for (int i = 0; i < csize; i++)
   {
     for (int j = 0; j < badcount; j++)
     {
@@ -141,19 +37,83 @@ void track_pf(int rresult[], int &pass, int &fail)
 bool game_continue(int rresult[])
 {
   int pass = 0, fail = 0;
-  track_pf(rresult, pass, fail)
+  track_pf(rresult, pass, fail);
   if (pass == 3 || fail == 3)
     return false;
   else
-    return true
+    return true;
 }
 
 bool final_result(int rresult[])
 {
   int pass = 0, fail = 0;
-  track_pf(rresult, pass, fail)
+  track_pf(rresult, pass, fail);
   if (pass >= 3)
     return true;
   else
     return false;
+}
+
+void hints(int arr[]){
+  srand((unsigned)time(NULL));
+  int hint=rand()%4;
+  cout<<"One of the bad guy is:"<<arr[hint]<<endl;
+}
+
+int main()
+{
+  cout<<"Welcome to Avalon. "<<endl;
+  int arr[badcount];
+  assign(arr);
+  cout<<"Loading successful. Game starts."<<endl;
+  int *rresult = new int [round];
+  for (int i = 0; i< round; i++)
+  {
+    int n = rcarsize[i];
+    cout << "This is round " <<i+1<<"."<<endl;
+    cout << "Please select " <<n<< " ministers to complete the task."<<endl;
+    int *currentcar = new int [n]; 
+    for (int j = 0; j < n; j++)
+    {
+      cin >> currentcar[j];
+    }
+    if (round_result(n, currentcar, arr))
+    {
+      rresult[i] = 1;
+      cout<<"Yay! This task is successful :)."<<endl;
+    }
+    else if (round_result(n, currentcar, arr) == 0)
+    {
+      rresult[i] = 2;
+      cout<<"Ouch! There is at least one bad guy in the team. Task failed :(."<<endl;
+      if (i>=1 && rresult[i-1]==2){
+        cout<<"After two successful sabotage, one of the bad ministers reveal flaws."<<endl;
+        cout<<"You have a chance to trace down, please select （Yes or No）"<<endl;
+        string ans;
+        cin>>ans;
+        if (ans=="Yes"){
+          hints(arr);
+        }
+      }
+    }
+    delete [] currentcar;
+    if (!game_continue(rresult))
+      break;
+
+  }
+ 
+  cout<<"Counting final results..."<<endl;
+  if (final_result(rresult))
+  {
+    cout<<"You win! XD"<<endl;
+  }
+  else if (!final_result(rresult))
+  {
+    cout<<"Sorry, you lost :("<<endl;
+  }
+  
+  delete [] rresult;
+  
+
+  return 0;
 }
