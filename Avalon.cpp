6b603efb43a873caf06eb6506287840f978b9d92
Scarlettidiot <99799956+Scarlettidiot@users.noreply.github.com>
2022-05-04 22:30:32 +0800

    Update Avalon.cpp

--- Avalon.cpp
+++ Avalon.cpp
@@ -68,3 +68,18 @@ bool final_result(int rresult[])
   else
     return false;
 }
+
+//randomly select 4 numbers to be bad guy.
+//input: the created array in main function
+//turn this array to an array stores 4 number representing the bad ministers.
+void assign(int arr[]){
+  srand((unsigned)time(NULL));
+  for (int i=0;i<4;i++){
+    arr[i]=rand()%10;//randomly select one number
+    for (int k=0;k<i;k++){
+      if (arr[k]==arr[i]){
+        i--;
+      }
+    }
+  }
+}
