d84b287565372d23a9b78cda037dd5540f512fae
TsukiYang <99883003+TsukiYang@users.noreply.github.com>
2022-05-04 22:42:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,51 +9,6 @@ const int round = 5; //the maximum round of the game.
 const int rcarsize[5] = {3, 4, 4, 5, 5}; //the restricted number of ministers in each round.
 
 
-bool round_result(int csize, int car[], int bad[])//determine and return the result of one round game. Parameter: how many ministers be chosen, which ministers be chosen, bad ministers array
-{
-  for (int i = 0; i < csize; i++)
-  {
-    for (int j = 0; j < badcount; j++)
-    {
-      if (car[i] == bad[j])
-      {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-void track_pf(int rresult[], int &pass, int &fail) //track the number of success and fail rounds to determine the final results.
-{
-  for (int i = 0; i < round; i++)
-  {
-    if (rresult[i] == 1)
-      pass++;
-    else if (rresult[i] == 2)
-      fail++;
-  }
-}
-bool game_continue(int rresult[]) //to determine whether we need to continue the game or the game has already had a result.
-{
-  int pass = 0, fail = 0;
-  track_pf(rresult, pass, fail);
-  if (pass == 3 || fail == 3)
-    return false;
-  else
-    return true;
-}
-
-bool final_result(int rresult[]) //to return the final result. (true for win, false for fail)
-{
-  int pass = 0, fail = 0;
-  track_pf(rresult, pass, fail);
-  if (pass >= 3)
-    return true;
-  else
-    return false;
-}
-
 void hints(int arr[]) //to randomly provide one bad minister's number when the player encounters 2 consecutive fail.
 { 
   srand((unsigned)time(NULL));
