8437039b2e3d4346242d504d04495d2229e24aa0
Scarlettidiot <99799956+Scarlettidiot@users.noreply.github.com>
2022-05-04 16:40:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,94 @@ round_result()//determine the e whether one round is success
 game_continue()//determine whether to continue a game
 final_result()//determinfinal result
   
+#include <iostream>
+#include <time.h>
+#include <string>
+using namespace std;
+
 int main(){
   cout<<"Welcome to Avalon!"<<endl;
+  int arr[4];
+  assign(arr[4]);
+  cout<<"Loading successful. Game starts."<<endl;
+  cout<<"ROUND I"<<endl;
+  cout<<"please select 3 minisers to join the task"<<endl;
+  int *a=new int[3];
+  for (int i=0;i<3;i++){
+    cin>>a[i];
+  }
+  if (round_result(a)==0){
+    cout<<"FAIL"<<endl;
+  }
+  else if (round_result(a)==1){
+    cout<<"SUCCESS!"<<endl;
+  }
+  delete [] a;
+  cout<<"ROUND II"<<endl;
+  cout<<"please select 4 minisers to join the task"<<endl;
+  int *a=new int[4];
+  for (int i=0;i<4;i++){
+    cin>>a[i];
+  }
+  if (round_result(a)==0){
+    cout<<"FAIL"<<endl;
+  }
+  else if (round_result(a)==1){
+    cout<<"SUCCESS!"<<endl;
+  }
+  delete [] a;
+  if (consecutive()=1){//means that two consecutive round occurs;
+    hints();
+  }
+  cout<<"ROUND III"<<endl;
+  cout<<"please select 4 minisers to join the task"<<endl;
+  int *a=new int[4];
+  for (int i=0;i<4;i++){
+    cin>>a[i];
+  }
+  if (round_result(a)==0){
+    cout<<"FAIL"<<endl;
+  }
+  else if (round_result(a)==1){
+    cout<<"SUCCESS!"<<endl;
+  }
+  delete [] a;
+  if (consecutive()=1){//means that two consecutive round occurs;
+    hints();
+  }
+  game_continue();
+  cout<<"ROUND IV"<<endl;
+  cout<<"please select 5 minisers to join the task"<<endl;
+  int *a=new int[5];
+  for (int i=0;i<5;i++){
+    cin>>a[i];
+  }
+  if (round_result(a)==0){
+    cout<<"FAIL"<<endl;
+  }
+  else if (round_result(a)==1){
+    cout<<"SUCCESS!"<<endl;
+  }
+  delete [] a;
+  if (consecutive()=1){//means that two consecutive round occurs;
+    hints();
+  }
+  game_continue();
+  cout<<"ROUND V"<<endl;
+  cout<<"please select 5 minisers to join the task"<<endl;
+  int *a=new int[5];
+  for (int i=0;i<5;i++){
+    cin>>a[i];
+  }
+  if (round_result(a)==0){
+    cout<<"FAIL"<<endl;
+  }
+  else if (round_result(a)==1){
+    cout<<"SUCCESS!"<<endl;
+  }
+  delete [] a;
+  final_result();
+  return 0;
 }
 
 
