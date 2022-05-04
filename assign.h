8b9d52cf91861c58318cb465db1f7b97f67fc790
Scarlettidiot <99799956+Scarlettidiot@users.noreply.github.com>
2022-05-04 19:15:07 +0800

    Update assign.h

--- assign.h
+++ assign.h
@@ -1,1 +1,11 @@
-
+void assign(int arr[]){
+  srand((unsigned)time(NULL));
+  for (int i=0;i<4;i++){
+    arr[i]=rand()%10;//randomly select one number
+    for (int k=0;k<i;k++){
+      if (arr[k]==arr[i]){
+        i--;
+      }
+    }
+  }
+}
