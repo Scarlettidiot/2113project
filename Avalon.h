c2c05f6fb25d0cdaf3a2c451bc40f03bd8515362
Scarlettidiot <99799956+Scarlettidiot@users.noreply.github.com>
2022-05-04 22:31:39 +0800

    Update Avalon.h

--- Avalon.h
+++ Avalon.h
@@ -2,3 +2,4 @@ bool round_result(int csize, int car[], int bad[]);
 void track_pf(int rresult[], int &pass, int &fail);
 bool game_continue(int rresult[]);
 bool final_result(int rresult[]);
+void assign(int arr[]);
