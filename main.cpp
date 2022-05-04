d33cc9ef22db245d620df9d29068e450be19ce53
Scarlettidiot <99799956+Scarlettidiot@users.noreply.github.com>
2022-05-04 15:14:53 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+
+assign()//assign numbers to bad, good, player
+select()//ask the player to choose numbers (a parameter to determine how many)
+round_result()//determine whether one round is success
+game_continue()//determine whether to continue a game
+final_result()//determine the final result
+  
+int main(){
+  cout<<"Welcome to Avalon!"<<endl;
+}
