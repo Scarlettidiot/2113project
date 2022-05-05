3ed45430ee24bd529c5a0b7a12feb9b27b977a7f
TsukiYang <99883003+TsukiYang@users.noreply.github.com>
2022-05-05 11:11:58 +0800

    update variable names

--- main.cpp
+++ main.cpp
@@ -13,42 +13,44 @@ const int rcarsize[5] = {3, 4, 4, 5, 5}; //the restricted number of ministers in
 int main()
 {
   cout<<"Welcome to Avalon. "<<endl;
-  int arr[badcount];
-  assign(arr);
+  int bad[badcount];
+  assign(bad);
   cout<<"Loading successful. Game starts."<<endl;
-  int *rresult = new int [round];
+  int *rresult = new int [round];//a dynamic array for storing game status for each round 
   for (int i = 0; i< round; i++)
   {
-    int n = rcarsize[i];
+    int csize = rcarsize[i];//variable storing the current group size of the round.
     cout << "This is round " <<i+1<<"."<<endl;
     cout << "(!-!) (@-@) (#-#) ($-$) (^-^) (&-&) (*-*) (>-<) (Q-Q) (T-T)" <<endl;
     cout << "  0     1     2     3     4     5     6     7     8     9  " <<endl;
-    cout << "Please select " <<n<< " ministers to complete the task."<<endl;
-    int *currentcar = new int [n]; 
-    for (int j = 0; j < n; j++)
+    cout << "Please select " <<csize<< " ministers to complete the task."<<endl;
+    int *currentcar = new int [csize]; //a dynamic array storing the selected numbers for each round. Will be updated in every new round.
+    for (int j = 0; j < csize; j++)
     {
       cin >> currentcar[j];
     }
-    if (round_result(n, currentcar, arr))
+    if (round_result(n, currentcar, bad))
     {
       rresult[i] = 1;
       cout<<"Yay! This task is successful :)."<<endl;
     }
-    else if (round_result(n, currentcar, arr) == 0)
+    else if (round_result(n, currentcar, bad) == 0)
     {
       rresult[i] = 2;
       cout<<"Ouch! There is at least one bad guy in the team. Task failed :(."<<endl;
-      if (i>=1 && rresult[i-1]==2){
+      if (i>=1 && rresult[i-1]==2)
+      {
         cout<<"After two successful sabotage, one of the bad ministers reveal flaws."<<endl;
         cout<<"You have a chance to trace down, please select （Yes or No）"<<endl;
         string ans;
         cin>>ans;
-        if (ans=="Yes"){
-          hints(arr);
+        if (ans=="Yes")
+        {
+          hints(bad);
         }
       }
     }
-    delete [] currentcar;
+    delete [] currentcar; //release the memory of player's selected numbers after each round.
     if (!game_continue(rresult))
       break;
 
@@ -64,7 +66,7 @@ int main()
     cout<<"Sorry, you lost :("<<endl;
   }
   
-  print_result(arr);
+  print_bad(bad);
   
   delete [] rresult;
   
